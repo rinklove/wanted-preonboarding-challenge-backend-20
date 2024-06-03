7a9decc6d36423f44477adc800a42cf32479878c
junho <>
2024-06-03 23:31:26 +0900

    fix: @Valid 어노테이션 적용

--- src/main/java/wanted/market/api/model/dto/member/SignupRequestDto.java
+++ src/main/java/wanted/market/api/model/dto/member/SignupRequestDto.java
@@ -1,6 +1,7 @@
 package wanted.market.api.model.dto.member;
 
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,16 @@ import lombok.ToString;
 @AllArgsConstructor
 public class SignupRequestDto {
 
+    @NotNull(message = "아이디는 필수 입력항목입니다.")
     private String memberId;
+
+    @NotNull(message = "비밀번호는 필수 입력항목입니다.")
     private String password;
+
+    @NotNull(message = "닉네임은 필수 입력항목입니다.")
     private String nickname;
+
+    @NotNull(message = "이메일은 필수 입력항목입니다.")
     private String email;
 
 }
